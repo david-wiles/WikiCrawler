73f13a9a7257efff390b055b5767fa3ad994f844
David Wiles <d.wiles7991@gmail.com>
2019-07-21 22:54:51 -0500

    test link extractors. List extractor and book extractor function, but how to filter out non-author links? At least half of extracted authors are not author pages

---
+++ book_scrapy/book_scrapy/spiders/test_spider.py
@@ -0,0 +1,12 @@
+from scrapy.spiders import Spider
+
+
+class TestSpider(Spider):
+
+    name = "TestSpider"
+
+    start_urls = ["file:///home/david/Desktop/List%20of%20historical%20novels%20-%20Wikipedia.html"]
+
+    def parse(self, response):
+        lists = response.css('div.mw-parser-output').xpath('./ul/li/a/@href').getall()
+        yield {'list': [('https://en.wikipedia.org' + link) for link in lists]}
